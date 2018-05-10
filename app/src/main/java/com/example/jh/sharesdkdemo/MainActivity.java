9868e4f1faa511559a635d3e1b5f9e8a364bd10a
jinhuizxc <1004260403@qq.com>
2018-05-10 15:06:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/sharesdkdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.sharesdkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 周末任务：把分享demo测试搞定！
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
